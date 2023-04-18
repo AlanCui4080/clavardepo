c90e1b96860b3711d7172f396998db6fd5099370
AlanCui4080 <me@alancui.cc>
2023-04-18 17:32:08 +0800

    feat: always_false

---
+++ src/include/clava/utility.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file utility.hpp
+ *
+ * @attention
+ * <h2> Copyright (C) 2023 clavardepo authors </h2>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General License for more details.
+ *
+ * You should have received a copy of the GNU Affero General License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/* SPDX-License-Identifier: AGPL-3.0-or-later */
+
+#include <type_traits>
+
+namespace clava
+{
+    template <class... T> struct always_false : std::false_type {
+    };
+    template <class... T>
+    constexpr auto always_false_v = always_false<T...>::value;
+}
